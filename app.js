0b14deaa2c39b37c9e3d2970910785c8faebfa26
Omer Yousaf <u.yusaf@gmail.com>
2019-09-18 13:00:49 +0500

    added /api prefix to all backend routes where the prefix was missing earlier

--- app.js
+++ app.js
@@ -44,15 +44,15 @@ app.use(bodyParser.urlencoded({ extended: false })); // omitting the "{ extended
 // warning "body-parser deprecated undefined extended: provide extended option" on executing the 'node app.js' command
 app.use(bodyParser.json());
 
-app.post('/authenticateLogin', routes.authenticateLogin);
-app.post('/isUsernameUnique', routes.isUsernameUnique);
-app.post('/forgotPassword', routes.forgotPassword);
-app.post('/resetPassword', routes.resetPassword);
-app.post('/upload-profile-image', MW.isLoggedIn, routes.uploadProfileImage);
-app.get('/getMember/:id', MW.isLoggedIn, routes.getMember);
-app.get('/profileimage', MW.isLoggedIn, routes.getProfileImage);
-app.get('/members', MW.isLoggedIn, routes.getMembers);
-app.put('/saveProgress', MW.isLoggedIn, routes.saveProgressRoute);
+app.post('/api/authenticateLogin', routes.authenticateLogin);
+app.post('/api/isUsernameUnique', routes.isUsernameUnique);
+app.post('/api/forgotPassword', routes.forgotPassword);
+app.post('/api/resetPassword', routes.resetPassword);
+app.post('/api/upload-profile-image', MW.isLoggedIn, routes.uploadProfileImage);
+app.get('/api/getMember/:id', MW.isLoggedIn, routes.getMember);
+app.get('/api/profileimage', MW.isLoggedIn, routes.getProfileImage);
+app.get('/api/members', MW.isLoggedIn, routes.getMembers);
+app.put('/api/saveProgress', MW.isLoggedIn, routes.saveProgressRoute);
 app.get('/api/logout', function(req, res){
   req.logout(); // method exposed by PassportJS, terminates user's session
   return res.status(config.App.httpStatuses.LOGOUT).send();
@@ -62,10 +62,6 @@ app.get('/api/events', calendarControllers.fetchAll);
 app.get('/api/events/:id', calendarControllers.fetchEvent);
 app.put('/api/events/:id', MW.isLoggedIn, calendarControllers.updateEvent);
 app.delete('/api/events/:id', MW.isLoggedIn, calendarControllers.deleteEvent);
-app.get('*', function(req, res) {
-    res.render('indexx.html'); // load the single view file from 'public' folder as that's been configured as the default
-    // lookup location for views. (after this first load, angular will handle the page changes on the front-end)
-});
 
 if(process.env.NODE_ENV !== 'test') { // spin off our http server only when NOT running in test env cuz the testing env sets one up automatically
 	app.set('port', process.env.PORT || config.App.server.port);
